0b1518783a2ad85e0308ee74c126487b5719a416
PSKoulouris <philippe.koulouris@gmail.com>
2024-12-27 13:15:44 +0100

    testing configuartion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:"#E7272D",
+        secondary:"#FBB217",
+      },
+      backgroundImage: {
+        "ardoise": "url('images/background-slate.jpg')", //trailing coma doest affect functionality but reduce error if new iten added to list
+      },
+    },
   },
   plugins: [],
 }
